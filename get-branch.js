01ec1b8235cc68925101ba0fa8d1df64d64cebb9
tnsc <antoniosapa90@gmail.com>
2024-09-23 20:28:33 +0200

    reverting custom script for deployment

--- get-branch.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require("child_process");
-
-// Get the current branch name
-const branch = execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
-
-// Output the branch name
-console.log(branch);
